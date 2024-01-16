b76009a42fe3d7068d9a87228c191f802b68a816
saad9alvi <156343613+saad9alvi@users.noreply.github.com>
2024-01-16 22:03:02 +0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+#integer
+integer=6
+print(integer,"is of type: ",type(integer))
+
+#string
+string="My Bro"
+print(string,"is of type: ",type(string))
+
+#float
+float=5.56
+print(float,"is of type: ",type(float))
+
+#boolean
+boolean= 6 == 5
+print(boolean,"is of type: ",type(boolean))
+
+#list
+list=[5,4,7]
+print(list,"is of type: ",type(list))
+
+#tuple
+tuple=(8,6,5,9,4)
+print(tuple,"is of type: ",type(tuple))
+
+#sets
+sets={75,67}
+print(sets,"is of type: ",type(sets))
+
+#dictionary
+dictionary={"name":"areeb","age":15}
+print(dictionary,"is of type: ",type(dictionary))
+
+ a= 5==5
+a=not True
+
